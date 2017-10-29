347c09ec487b3eab6ee3b17f2cae2e397cd35126
Dan Reynolds <me@danreynolds.ca>
2017-10-29 12:49:59 -0700

    add deploy option

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,9 @@
 if [ "$DEPLOY_TYPE" = "deploy" ]; then
   echo "Deploying version $DEPLOY_TAG."
   rake docker:deploy
+elif [ "$DEPLOY_TYPE" = "build" ]; then
+  echo "Building version $DEPLOY_TAG."
+  rake docker:build
 else
   echo "Building new version $DEPLOY_TAG and deploying."
   rake docker:build_deploy
